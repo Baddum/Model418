6b24f5fd0ba2364a1d643c5b6dc768e45fbd8c80
Thomas ZILLIOX <thomas@zilliox.me>
2014-11-18 19:05:50 +0100

    Improve example page output

--- example/index.php
+++ example/index.php
@@ -4,17 +4,27 @@ session_start();
 
 // Require composer autoload
 require_once(__DIR__ . '/../vendor/autoload.php');
-use Elephant418\Model418\Example\BasicUsage\UserModel;
-// use Elephant418\Model418\Example\JSONProvider\UserModel;
+//use Elephant418\Model418\Example\BasicUsage\UserModel;
+use Elephant418\Model418\Example\JSONProvider\UserModel;
 // use Elephant418\Model418\Example\SessionProvider\UserModel;
+
+function dumpUser($user) {
+    return $user->firstName.' '.$user->lastName.' ('.$user->id.')'.PHP_EOL;
+}
 
 // Retrieve all models
 $userList = (new UserModel)->query()->fetchAll();
+foreach ($userList as $user) {
+    echo 'Existing: '.dumpUser($user);
+}
+echo '---'.PHP_EOL;
 
 // Save a new Model
 $user = (new UserModel)
     ->set('firstName', 'John')
     ->save();
+
+echo 'Added:    '.dumpUser($user);
 
 // Retrieve by primary key
 $johnId = $user->id;
@@ -27,7 +37,7 @@ $john->set('lastName', 'Doe')
     ->save();
 
 // Access to Model attribute
-echo $john->firstName.' '.$john->lastName.PHP_EOL;
+echo 'Updated:  '.dumpUser($john);
 
 // Delete an existing Model
 $john->delete();
